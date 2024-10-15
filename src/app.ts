57fd1309c24f0d41fb1f98521eb6ab12651a335c
sahildhiman <thesahildhiman@gmail.com>
2024-10-15 21:21:49 +0530

    global error handler added

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,25 @@
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
+import { HttpError } from "http-errors";
+import globalErrorHandler from "./middleware/globalErrorHandler";
 
 const app = express();
+
+// routes
+app.use("/", () => {});
+
+// middleware- function that placed between router and handler
+// global error handler - should be placed at last after all routes
+// sometimes we got error in req-handler, then control goes to global error handler which sends the response to client accordingly
+// error handler contains 4 parameters- err, req, res, next
+
+// global error handler
+app.use(
+  globalErrorHandler as (
+    err: HttpError,
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ) => void
+);
 
 export default app;
---
+++ src/middleware/globalErrorHandler.ts
@@ -0,0 +1,14 @@
+import { NextFunction, Request, Response } from "express";
+import { HttpError } from "http-errors";
+
+const globalErrorHandler = (
+  err: HttpError,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const statusCode = err.statusCode || 500;
+  return res.status(statusCode).json({ message: err.message });
+};
+
+export default globalErrorHandler;
