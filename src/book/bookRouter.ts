696bc5c2faf3f12df7e7ca6efdf43cd7e53bb426
sahildhiman <thesahildhiman@gmail.com>
2024-10-18 12:35:33 +0530

    setup multer middleware

--- src/book/bookRouter.ts
+++ src/book/bookRouter.ts
@@ -1,9 +1,27 @@
 import express from "express";
 import { createBook } from "./bookController";
+import multer from "multer";
+import path from "path";
 
 const bookRouter = express.Router();
 
+// file store local - middleware
+const upload = multer({
+  dest: path.resolve(__dirname, "../../public/uploads"),
+  limits: { fileSize: 3e7 },
+});
+
 // routes
-bookRouter.post("/create", createBook as any);
+bookRouter.post(
+  "/create",
+  upload.fields([
+    { name: "coverImage", maxCount: 1 },
+    { name: "file", maxCount: 1 },
+  ]),
+  createBook as any
+);
 
 export default bookRouter;
+// upload.single() - used to upload single file
+// upload.fields() - used to array of files in multipart/form data
+// multer first of all stores uploaded files locally and then auto delete these files
